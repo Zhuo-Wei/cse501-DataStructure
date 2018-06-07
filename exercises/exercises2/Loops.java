f3d60162ea7c5e4b0e9fdaf13bdd6faf5878f4cd
DESKTOP-39VDHIP\longf <f.long@wustl.edu>
2018-06-07 11:26:05 -0500

    finish

--- exercises/exercises2/Loops.java
+++ exercises/exercises2/Loops.java
@@ -48,9 +48,11 @@ public class Loops {
 		// 0 to 9 inclusively
 		//    produces  0 1 2 3 4 5 6 7 8 9
 		int t = 0;
-				while (t<=9);
-				 t= t+1;
-		System.out.println( t);
+		while (t<=9);{
+				System.out.println( t+"" );
+				t= t+1;
+				}
+		System.out.println();
 		
 		// 1 to 10 inclusively
 		//    produces 1 2 3 4 5 6 7 8 9 10
