efd5e09031249fa57503f3dab8b65abb09106333
weizhuo <weizhuo@weizhuo-pc>
2018-06-18 14:05:37 -0500

    finish

--- extensions/arrays/Birthday.java
+++ extensions/arrays/Birthday.java
@@ -4,7 +4,17 @@ import cse131.ArgsProcessor;
 public class Birthday {
 	
 	public static void main(String[] args) {
-		
+		ArgsProcessor ap = new ArgsProcessor(args);	
+	int N = ap.nextInt("How many people?");
+	int[][]cal= new int[12][31];
+			for(int i= 0; i<N; i++) {
+				int m =(int)(Math.random()*12);
+				int d =(int)(Math.random()*31);
+			    cal[m][d]=cal[m][d]+ 1;
+			    System.out.print(cal[m][d]);
+			}
+	
+	
 	}
 
 }
--- extensions/arrays/PascalsTriangle.java
+++ extensions/arrays/PascalsTriangle.java
@@ -4,7 +4,48 @@ import cse131.ArgsProcessor;
 public class PascalsTriangle {
 	
 	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);	
+		int N = ap.nextInt("What is the number of rows?");
 		
+		int[][] pt = new int[N][N];
+		
+		for(int i = 0; i <N; i++) {
+			for (int j= 0; j <N; j++) {
+			
+					pt[i][j]= 1;
+				}
+			}
+		
+				
+		for(int r = 0; r <N; r++) {
+			for (int c = 0; c <N; c++) {	
+					if (c>=1&&r>1) {
+						pt[r][c] = pt[r-1][c]+ pt[r-1][c-1];
+						if (c==0||c==r) {
+							pt[r][c]=1;
+							}
+					}
+					int entry = pt[r][c];
+					if (c>r) {
+						System.out.print("   "); 
+					}
+					
+					 else 
+						 if (entry>=10) {
+							 System.out.print(" " + pt[r][c] );
+						 }
+						 else System.out.print(" "+ pt[r][c] + " ");
+					
+					
+			}	
+			System.out.println();
+		}
+				
+					
+						
+			
+		
+	
 	}
 
 }
