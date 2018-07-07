add7c80432a49149bc6362bff87ee264ea828a07
weizhuo <weizhuo@weizhuo-pc>
2018-07-07 01:25:06 -0500

    11

--- labs/lab5/Lab5Methods.java
+++ labs/lab5/Lab5Methods.java
@@ -1,7 +1,37 @@
 package lab5;
 
+import static org.junit.Assert.assertEquals;
+
 public class Lab5Methods {
 
-
-
+	public static int sumDownBy2(int n) {
+	
+		int sum = 0;
+		while(n >= 0){
+			sum += n;
+			n -= 2;
+		}
+		return sum;
+	}
+	public static double harmonicSum(int n) {
+		double sum = 0;
+		while(n>0) {
+			for(int i = 1; i < n+1; i++) {
+				sum = sum+1.0/i;
+			}
+		}
+		return sum;
+	}
+	public static double geometricSum(int k) {
+		//RETURN VALUE: the sum 1 + 1/2 + 1/4 + 1/8 + .. + 1/Math.pow(2,k)
+		int sum =0;
+		while(k >= 0){
+			sum += (1 / Math.pow(2, k));
+			k--;
+		}
+		return sum;
+	}
+	
+	accomplish this with
+	
 }
