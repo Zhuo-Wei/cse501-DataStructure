07c28e62cbc0bc507eaca704c6bef9f745a72932
weizhuo <weizhuo@weizhuo-pc>
2018-07-07 22:56:31 -0500

    lab 5 finish

--- labs/lab5/Lab5Methods.java
+++ labs/lab5/Lab5Methods.java
@@ -32,6 +32,35 @@ public class Lab5Methods {
 		return sum;
 	}
 	
-	accomplish this with
+	public static int multPos(int k, int j) {
+		int sum=0;
+		int i=0;
+		while( i<j) {
+			sum=sum+k;
+			i++;
+		}
+		return sum;
+	}
 	
+	public static int mult(int k, int j) {
+		int sum=0;
+		int i=0;
+		if (k!=0&&j!=0) {
+			while( i<j) {
+				sum=sum+k;
+				i++;
+			}
+		}
+		else sum=0;
+		return sum;
+	}
+	public static int expt(int n, int k) {
+		int sum=1;
+		if (k>0) {
+			while(k>0) {
+				sum = sum*k;
+			}
+		}
+		return sum;
+	}
 }
