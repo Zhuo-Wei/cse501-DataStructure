99f67f45ef8b10e58a8227d660426eb7fa485284
weizhuo <weizhuo@weizhuo-pc.dhcp.wustl.edu>
2018-07-15 21:39:51 -0500

    lab6 finish

--- labs/lab6/Methods.java
+++ labs/lab6/Methods.java
@@ -1,19 +1,59 @@
 package lab6;
 
+import cse131.ArgsProcessor;
+
 public class Methods {
 	
-	//
-	// In this class, implement the f and g functions described on the lab page
-	//
-
+	//f(x) =    x-10     if x > 100
+    //f(f(x+11))  if x <= 100
+	
+	public static int f(int x) {
+		if (x>100) {
+			return x-10;
+		}
+		else {
+			return f(f(x+11));
+		}
+	}
+	//g(x,y)  = y+1               if x = 0
+       // = g(x-1,1)          if x > 0 and y = 0
+       // = g(x-1, g(x, y-1)) if x > 0 and y > 0	
+	public static int g(int x, int y) {
+		if(x==0) {
+			return (y+1);	
+		}
+		else {
+			if (x>0&&y==0) {
+				return g(x-1,1);
+			}
+			else { 
+				if(x>0&&y>0) {
+					return g(x-1, g(x, y-1));
+			}
+				else return 0;
+			}
+		} 
+	}
+		
+	
+	
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		//
-		// from here, call f or g with the appropriate parameters
-		//
+		ArgsProcessor ap = new ArgsProcessor(args);
+
+	    System.out.println(f(99));
+
+
+	    System.out.println(g(1,0));
+
+	    System.out.println( g(1,2));
+
+	    System.out.println(g(2,2));
+
 
 	}
 
---
+++ labs/lab6/Triangles.java
@@ -0,0 +1,71 @@
+package lab6;
+
+import java.awt.Color;
+
+import cse131.ArgsProcessor;
+import sedgewick.StdDraw;
+
+public class Triangles {
+
+	public static void tri(double[]x,double[]y) {
+		if(x[1]-x[0]<0.01) {
+			return;
+		}
+		StdDraw.setPenColor(Color.white);
+		double[] a=new double[3];
+		a[0]=(x[0]+x[1])/2;
+		a[1]=(x[1]+x[2])/2;
+		a[2]=(x[2]+x[0])/2;
+		double[] b=new double[3];
+		b[0]=(y[0]+y[1])/2;
+		b[1]=(y[1]+y[2])/2;
+		b[2]=(y[2]+y[0])/2;
+		StdDraw.filledPolygon(a, b);
+		double[] x1=new double[3];
+		x1[0]=x[0];
+		x1[1]=a[0];
+		x1[2]=a[2];
+		double[] x2=new double[3];
+		x2[0]=a[0];
+		x2[1]=x[1];
+		x2[2]=a[1];
+		double[] x3=new double[3];
+		x3[0]=a[2];
+		x3[1]=a[1];
+		x3[2]=x[2];
+		double[] y1=new double[3];
+		y1[0]=y[0];
+		y1[1]=b[0];
+		y1[2]=b[2];	
+		double[] y2=new double[3];
+		y2[0]=b[0];
+		y2[1]=y[1];
+	    y2[2]=b[1];	
+		double[] y3=new double[3];
+		y3[0]=b[2];
+		y3[1]=b[1];
+	    y3[2]=y[2];	
+		
+		tri(x1,y1);
+		tri(x2,y2);
+		tri(x3,y3);
+		StdDraw.pause(100);
+	}
+	
+	public static void main(String[] args) {
+		ArgsProcessor ap = new ArgsProcessor(args);
+		double[] x =new double[3];
+		double[] y =new double[3];
+		x[0]=0;
+		x[1]=0.5;
+		x[2]=1;
+		y[0]=0;
+		y[1]=0.866;
+		y[2]=0;
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledPolygon(x, y);
+		tri(x,y);
+
+	}
+
+}
