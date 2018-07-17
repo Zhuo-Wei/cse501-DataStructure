42ff48756305e84a9ac1bbb8c8c98837a6880005
weizhuo <weizhuo@weizhuo-pc.home>
2018-07-17 17:52:12 -0500

    ext 1!!!

--- extensions/imageprocessor/Filters.java
+++ extensions/imageprocessor/Filters.java
@@ -30,7 +30,12 @@ public class Filters {
 	// This method takes the color of each pixel and creates a new color without any green.  Returns an array of integers [r, g ,b].
 	// USED IN: example_purplish
 	public static Color purplish(Color c) {
-		return Color.black;  // FIXME
+		int red = c.getRed(); 
+	
+		int blue = c.getBlue();
+		
+		Color n = new Color(red,0,blue);
+		return n;  
 	}
 
 	// Now that you've seen the examples, complete the following methods.
@@ -44,55 +49,83 @@ public class Filters {
 	 *the first source image to the target panel.  
 	 *(Hint: This is a very simple method.)**/
 	public static int copy(int pixelComponent) {
-		return 0;  // FIXME
+		return pixelComponent; 
 	}
 
 	//This method averages the color components of two pixels.
 	// USED IN: composite
-	public static int composite(int a, int b) {
-		return 0;  // FIXME
-	}
+	public static int composite(int pixelAComponent, int pixelBComponent) {
+			return (pixelAComponent+pixelBComponent)/2;
+		}
+	
 
 	//This method returns the negative of a pixel by inverting its color components.
 	// USED IN: negative
-	public static int negative(int a) {
-		return 0;  // FIXME
+	public static int negative(int pixelComponent) {
+		return (255-pixelComponent);  // FIXME
 	}
 
 	//This method reduces the number of possible values for a given color component
 	//from 256 to 2, by returning either 0 or 255 based on the original value.
 	// USED IN: posterize
 	public static int posterize(int a) {
-		return 0;   // FIXME
+		int x=a/255;
+		return x*255;
 	}
 
 	//This method returns a color that is brighter than the original color.
 	// USED IN: brighter
 	//FIX ME
 	public static Color brighter(Color c) {
-		return Color.black;  // FIXME
+		return c.brighter();  
 	}
 
 	//This method returns a color that is some shade of gray, by making a new
 	//color having equal RGB components. returns an array of integers [r, g ,b].
 	// USED IN: grayscale
 	public static Color grayscale(Color c) {
-		return Color.black;  // FIXME
+		int red = c.getRed(); 
+		int green = c.getGreen(); 
+		int blue = c.getBlue();
+		int gray=(red+ green+ blue)/3;
+		Color n = new Color(gray, gray, gray);
+		
+		return n; 
 	}
 
 	//This method returns either black or white, based on the intensity of the
 	//originally provided color. returns an array of integers [r, g ,b].
 	// USED IN: blackWhite
 	public static Color blackAndWhite(Color c) {
-		return Color.black;   // FIXME
+		int red = c.getRed(); 
+		int green = c.getGreen(); 
+		int blue = c.getBlue();
+		int ave= (red+ green+ blue)/3;
+		if(ave>127) {
+			return Color.white;
+		}
+		else{
+			return Color.black;	
+		}   
 	}
 
 	//This method combines two images by choosing for each location the brighter 
 	//pixel in the same location from the two source images.
 	// USED IN: combineBrighter
 	public static Color combineBrighter(Color c1, Color c2) {
-		return Color.black;  // FIXME
-
+		int red1 = c1.getRed(); 
+		int green1 = c1.getGreen(); 
+		int blue1 = c1.getBlue();
+		int sum1= red1+ green1+ blue1;
+		int red2 = c2.getRed(); 
+		int green2 = c2.getGreen(); 
+		int blue2 = c2.getBlue();
+		int sum2= red2+ green2+ blue2;
+		if(sum1>sum2) {
+			return c1;
+		}
+		else return c2;
+		
 
 	}
 	/**This is the beginning of another extension*
