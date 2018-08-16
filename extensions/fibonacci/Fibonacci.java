b5c2bc6e5375eff7b316b9ee45f522420304a4b7
alva <alva@weizhuos-mbp>
2018-08-16 00:50:54 -0500

    6.4 finish

--- extensions/fibonacci/Fibonacci.java
+++ extensions/fibonacci/Fibonacci.java
@@ -8,7 +8,27 @@ public class Fibonacci {
 	 * @return fib(n), computed recursively
 	 */
 	public static int recursive(int n) {
-		return 0; // FIXME
+		if(n==0) {
+			return 0;
+		}
+		if(n==1) {
+			return 1;
+		}
+		else {
+			int x=1;
+			int y=1;
+			int sum=x+y;
+		
+			while(n>3) {
+				x=y;
+				y=sum;
+				sum = sum+ x ;
+				n--;
+		
+			}
+			return sum	;	
+		}
+		
 	}
 	
 	/**
@@ -17,7 +37,24 @@ public class Fibonacci {
 	 * @return fib(n), computed iteratively
 	 */
 	public static int iterative(int n) {
-		return 0; // FIXME
+		if(n==0) {
+			return 0;
+		}
+		if(n==1) {
+			return 1;
+		}
+		int a=1;
+		int b=1;
+		int Sum=a+b;
+		int c=0;
+		for(int i=0; i<n-3;i++) {
+			c=Sum;
+			Sum= Sum+b;
+			b=c;
+		}
+		return Sum;
 	}
-
+	
 }
+
+
